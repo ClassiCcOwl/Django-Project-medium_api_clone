180afaa33e1878f5b2eaa58a5de22445680c3692
ClassiCcOwl <khavari.7878@yahoo.com>
2024-04-03 14:32:43 +0330

    local and production settings configed

---
+++ author_api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *  # noqa
+from .base import env
+
+ADMINS = [("Mohammad Khavari", "khavari.7878@yahoo.com")]
+
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = []
