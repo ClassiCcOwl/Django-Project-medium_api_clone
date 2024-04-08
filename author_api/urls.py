0025e548bd482e0fe64c64fcead79400011ad836
ClassiCcOwl <khavari.7878@yahoo.com>
2024-04-08 14:14:13 +0330

    feat: add custom user model, setup redoc api docs

--- author_api/urls.py
+++ author_api/urls.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors Haven api",
+        default_version="V1",
+        description="Api endpopints for authors haven",
+        contact=openapi.Contact(email="khavari.7878@yahoo.com"),
+        license=openapi.License(name="MIT Licence"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+
+admin.site.site_header = "Authors Heaven API Admin"
+admin.site.site_title = "Authors Heaven API Admin Portal"
+admin.site.index_title = "Welcome to Authors Haven API Portal"
