63e382218ffa34ed1f028d4403a02931b4e26468
ClassiCcOwl <khavari.7878@yahoo.com>
2024-04-15 14:38:02 +0330

    feat(profile): profile, follow, unfollow views and serializers

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -1,3 +1,151 @@
-from django.shortcuts import render
+# TODO: change this in production
+from author_api.settings.local import DEFAULT_FROM_EMAIL
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser
 
-# Create your views here.
+from .exceptions import CantFollowYourself
+from .models import Profile
+from .pagination import ProfilePagination
+from .renderers import ProfileJSONREnderer, ProfilesJSONREnderer
+from .serializers import ProfileSerializer, FollowingSerializer, UpdateProfileSerializer
+
+User = get_user_model()
+
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    pagination_class = ProfilePagination
+    renderer_classes = (ProfilesJSONREnderer,)
+
+
+class ProfileDetailAPIView(generics.RetrieveAPIView):
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+    renderer_classes = (ProfileJSONREnderer,)
+
+    def get_queryset(self):
+        return Profile.objects.select_related("user")
+
+    def get_object(self):
+        user = self.request.user
+        profile = self.get_queryset().get(user=user)
+        return profile
+
+
+class UpdateProfileAPIView(generics.RetrieveAPIView):
+    serializer_class = UpdateProfileSerializer
+    permission_classes = (IsAuthenticated,)
+    parser_classes = (MultiPartParser,)
+    renderer_classes = (ProfileJSONREnderer,)
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class FollowerLisView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, requst, format=None):
+
+        try:
+            profile = Profile.objects.get(user__id=requst.user.id)
+            follower_profiles = profile.followers.all()
+            serilizer = FollowingSerializer(follower_profiles, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "followers_count": follower_profiles.count(),
+                "followers": serilizer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=404)
+
+
+class FollowingLisView(APIView):
+    # permission_classes = (IsAuthenticated,)
+
+    def get(self, requst, user_id, format=None):
+
+        try:
+            profile = Profile.objects.get(user__id=user_id)
+            following_profiles = profile.following.all()
+            users = [p.user for p in following_profiles]
+            serilizer = FollowingSerializer(users, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "following_count": following_profiles.count(),
+                "users_i_follow": serilizer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=404)
+
+
+class FollowAPIView(APIView):
+    def post(self, request, user_id, format=None):
+        try:
+            follower = Profile.objects.get(user=self.request.user)
+            user_profile = request.user.profile
+            profile = Profile.objects.get(user__id=user_id)
+            if profile == follower:
+                raise CantFollowYourself
+
+            if user_profile.check_following(profile):
+                formatted_response = {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": f"You are already following {profile.user.first_name} {profile.user.last_name}",
+                }
+                return Response(formatted_response, status=status.HTTP_400_BAD_REQUEST)
+
+            user_profile.follow(profile)
+            subject = "A new user follows you"
+            message = f"Hi there, {profile.user.first_name}!!, the user {user_profile.user.first_name} {user_profile.user.last_name} now follows you"
+            from_email = DEFAULT_FROM_EMAIL
+            recipient_list = [profile.user.email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+
+            return Response(
+                {
+                    "status_code": status.HTTP_200_OK,
+                    "message": f"You are now following {profile.user.first_name} {profile.user.last_name}",
+                }
+            )
+
+        except Profile.DoesNotExist:
+            raise NotFound("You can't follow a profile that does not exist.")
+
+
+class UnfollowAPIView(APIView):
+    def post(self, request, user_id, *args, **kwargs):
+        user_profile = request.user.profile
+        profile = Profile.objects.get(user__id=user_id)
+
+        if not user_profile.check_following(profile):
+            formatted_response = {
+                "status_code": status.HTTP_400_BAD_REQUEST,
+                "message": f"You can't unfollow {profile.user.first_name} {profile.user.last_name}, since you were not following then in the first place",
+            }
+            return Response(formatted_response, status.HTTP_400_BAD_REQUEST)
+        user_profile.unfollow(profile)
+
+        formatted_response = {
+            "status_code": status.HTTP_200_OK,
+            "message": f"You have unfollowed {profile.user.first_name} {profile.user.last_name}",
+        }
+        return Response(formatted_response, status.HTTP_200_OK)
